1ef8a280ed66b68f72d8e7e0313fe80db5795277
Diksha Aswal <diksha.aswal94@gmail.com>
2020-09-08 13:28:52 +0530

    two functions added

--- Code.py
+++ Code.py
@@ -1,2 +1,19 @@
-import numpy as np
-print hello
+# verifies if the game is completed
+def check_if_game_over():
+  check_for_winner()
+  check_for_tie()
+
+# finds if any player is the winner
+def check_for_winner():
+  global winner
+  row_winner = check_rows()
+  column_winner = check_columns()
+  diagonal_winner = check_diagonals()
+  if row_winner:
+    winner = row_winner
+  elif column_winner:
+    winner = column_winner
+  elif diagonal_winner:
+    winner = diagonal_winner
+  else:
+    winner = Non
