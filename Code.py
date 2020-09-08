e1c2e1af7d3c89e873c5913dcb984abfa796cc21
Saket232 <saketlakh@gmail.com>
2020-09-08 13:33:05 +0530

    Modified code by saket first time

--- Code.py
+++ Code.py
@@ -1,2 +1,33 @@
-import numpy as np
-print hello
+# checks if any horizontal x/o pattern is present
+def check_rows():
+  global game_still_going
+  row_1 = board[0] == board[1] == board[2] != "-"
+  row_2 = board[3] == board[4] == board[5] != "-"
+  row_3 = board[6] == board[7] == board[8] != "-"
+  if row_1 or row_2 or row_3:
+    game_still_going = False
+  if row_1:
+    return board[0]
+  elif row_2:
+    return board[3]
+  elif row_3:
+    return board[6]
+  else:
+    return None
+
+# checks if any vertical x/o pattern is present
+def check_columns():
+  global game_still_going
+  column_1 = board[0] == board[3] == board[6] != "-"
+  column_2 = board[1] == board[4] == board[7] != "-"
+  column_3 = board[2] == board[5] == board[8] != "-"
+  if column_1 or column_2 or column_3:
+    game_still_going = False
+  if column_1:
+    return board[0]
+  elif column_2:
+    return board[1]
+  elif column_3:
+    return board[2]
+  else:
+    return None
